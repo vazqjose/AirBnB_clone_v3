47820fd7cf1632b722f73bedfa163e04f2c5fa58
Jose Vazquez <vazqjose@gmail.com>
2021-09-26 02:52:52 +0000

    checklist items

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,23 +1,17 @@
 $(document).ready(function() {
 
   let amenities = [];
-	let prettyArr = [];
- 
+
   $(':checkbox').change(function() {
 
     let name = $(this).attr("data-name");
 
     if (this.checked) {
       amenities.push(name);
-      prettyArr.push(' ' + name);
     } else {
-      indexToDel = $.inArray(name, amenities);
-      amenities.splice(indexToDel, 1);
-      prettyArr.splice(indexToDel, 1);
+      amenities.splice($.inArray(name, amenities), 1);
     }
-
-    $("h4").text(prettyArr);
+    $("h4").text(amenities.join(', '));
   });
 
 });
-
