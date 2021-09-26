13ff90ecdd3c991d08b598a948a54eb7f78fa707
Jose Vazquez <vazqjose@gmail.com>
2021-09-26 04:00:09 +0000

    added array of ids

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
-
   let amenities = [];
-
+  let idArr = [];
   $(':checkbox').change(function() {
-
     let name = $(this).attr("data-name");
-
+    let id = $(this).attr("data-id");
     if (this.checked) {
       amenities.push(name);
+      idArr.push(id);
     } else {
       amenities.splice($.inArray(name, amenities), 1);
+      idArr.splice($.inArray(id, idArr), 1);
     }
     $("h4").text(amenities.join(', '));
   });
-
 });
