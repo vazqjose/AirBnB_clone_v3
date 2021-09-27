f2d044cc59c863d0c6e7915f471930969d170aa7
Jose Vazquez Cruz <vazqjose@gmail.com>
2021-09-27 18:09:33 -0400

    Update 1-hbnb.js

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -15,20 +15,3 @@ $(document).ready(function() {
     $(".amenities > h4").text(amenities.join(', '));
   });
 });
-/*
-$(function () {
-	const dictamenities = {};
-	const checkboxes = $('input');
-	for (const box of checkboxes) {
-		box.addEventListener('change', function () {
-			if (box.checked) {
-				dictamenities[$(box).data('id')] = $(box).data('name');
-			} else {
-				delete dictamenities[$(box).data('id')];
-			}
-			let listamenities = Object.values(dictamenities);
-			$('DIV.amenities h4').text(listamenities.join(', '));
-		});
-	}
-});
-*/
