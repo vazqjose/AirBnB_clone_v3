1c107c059d08fd7e55bb232a7cca9a1ef6bf31e4
Thegermanblob <thegermanblob@gmail.com>
2021-10-04 15:32:14 -0400

    Add task 5

---
+++ web_dynamic/static/scripts/4-hbnb.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+  let amenities = [];
+  let idArr = [];
+  $(':checkbox').change(function () {
+    let name = $(this).attr("data-name");
+    let id = $(this).attr("data-id");
+    if (this.checked) {
+      amenities.push(name);
+      idArr.push(id);
+    } else {
+      amenities.splice($.inArray(name, amenities), 1);
+      idArr.splice($.inArray(id, idArr), 1);
+    }
+    $(".amenities h4").text(amenities.join(', '));
+  });
+  $.ajax({
+    type: "POST",
+    url: " http://0.0.0.0:5001/api/v1/places_search/",
+    data: "{}",
+    contentType: 'application/json',
+    dataType: 'json',
+    success: postPlaces
+  });
+
+  $('BUTTON').click(function () {
+    $.ajax({
+      url: api + ':5000/api/v1/places_search',
+      type: 'POST',
+      data: JSON.stringify({ 'amenities': Object.amenities }),
+      contentType: 'application/json',
+      dataType: 'json',
+      success: postPlaces
+
+    });
+  });
+
+
+    $.get("http://0.0.0.0:5001/api/v1/status/", function (data, myStatus) {
+      if (myStatus == 'success')
+        $("div#api_status").addClass('available');
+      else
+        $("div#api_status").removeClass('available');
+    });
+
+  function postPlaces (places_json) {
+    $('SECTION.places').append(places_json.map(place => {
+      return `<article>
+    <div class="title_box">
+      <h2>${place.name}</h2>
+      <div class="price_by_night">${place.price_by_night}</div>
+    </div>
+    <div class="information">
+      <div class="max_guest">${place.max_guest} Guests</div>
+            <div class="number_rooms">${place.number_rooms} Bedrooms</div>
+            <div class="number_bathrooms">${place.number_bathrooms} Bathrooms</div>
+    </div>
+          <div class="description">
+      ${place.description}
+          </div>
+  </article>`;
+    }))
+  }
+});
